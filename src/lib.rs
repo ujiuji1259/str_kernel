e9b7ea076b56730b413397464bbf01d9b9679262
ujiuji1259 <suzzz428@gmail.com>
2021-08-04 09:07:15 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pymodule]
+fn str_kernel(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
